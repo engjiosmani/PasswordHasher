7563cdab57c52268be01525ee4448c6d0c25d98c
engjiosmani <154840265+engjiosmani@users.noreply.github.com>
2025-05-18 16:04:29 +0200

    PasswordHasher

---
+++ src/main/java/utils/PasswordHasher.java
@@ -0,0 +1,23 @@
+package utils;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+
+public class PasswordHasher {
+
+
+    private static final int ITERATIONS = 65536;
+    private static final int KEY_LENGTH = 256;
+    private static final String ALGORITHM = "PBKDF2WithHmacSHA256";
+
+    public static String getFullHashingProcessExplanation(String password) {
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            byte[] saltBytes = generateSalt();
+            String saltBase64 = encodeSalt(saltBytes);
+            
