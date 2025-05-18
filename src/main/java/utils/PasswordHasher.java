21a33a3fee7614384955efa3fea3e405d2e8bca4
Enkel Berisha <enkel.berisha@student.uni-pr.edu>
2025-05-18 17:03:08 +0200

    updated PasswordHasher.java

--- src/main/java/utils/PasswordHasher.java
+++ src/main/java/utils/PasswordHasher.java
@@ -20,3 +20,23 @@ public class PasswordHasher {
         try {
             byte[] saltBytes = generateSalt();
             String saltBase64 = encodeSalt(saltBytes);
+
+            PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), saltBytes, ITERATIONS, KEY_LENGTH);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance(ALGORITHM);
+            byte[] hashBytes = factory.generateSecret(spec).getEncoded();
+            String hashBase64 = Base64.getEncoder().encodeToString(hashBytes);
+
+            sb.append("PASSWORD HASHING PROCESS\n");
+            sb.append("------------------------------\n");
+            sb.append("1. Input Password: ").append(password).append("\n\n");
+
+            sb.append("2. Generated Salt (Raw Bytes):\n");
+            for (byte b : saltBytes) {
+                sb.append(String.format("%02x ", b));
+            }
+            sb.append("\n\n");
+
+            sb.append("3. Generated Salt (Base64): ").append(saltBase64).append("\n\n");
+            sb.append("4. Algorithm: ").append(ALGORITHM).append("\n\n");
+            sb.append("5. Iterations: ").append(ITERATIONS).append("\n\n");
+            sb.append("6. Key Length: ").append(KEY_LENGTH).append(" bits\n\n");
